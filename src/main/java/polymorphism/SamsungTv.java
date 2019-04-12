e6883ab753b7053a13902a12ce5ca45791411760
일반 <@DESKTOP-I43I40G>
2019-04-12 13:04:21 +0900

    4/12 IoC의존성

--- src/main/java/polymorphism/SamsungTv.java
+++ src/main/java/polymorphism/SamsungTv.java
@@ -1,20 +1,50 @@
 package polymorphism;
 
 public class SamsungTv  implements TV {
+	
+	private SonySpeaker speaker;
+	
+	public SamsungTv() {
+		System.out.println("===> SamsungTv(1) 생성자(기본) --- 객체 생성");
+	}
+	
+	//생성자 오버로딩 -> 생성자 인젝션
+	public SamsungTv(SonySpeaker speaker) {
+		System.out.println("===> SmasungTv(2) 생성자(오버로딩) --- 객체 생성");
+		this.speaker = speaker;
+	}
+	
+	//init-method 속성
+	public void initMethod() {
+		System.out.println("initMethod --- 객체 초기화 작업 처리");
+	}
+	
+	// destroy-method 속성
+	public void destroyMethod() {
+		System.out.println("destroyMehod --- 객체 삭제 전에 처리할 로직 처리");
+	}
+	
 	@Override
 	public void powerOn() {
 		System.out.println("SamsungTv---전원 켜다");
 	}
+	
 	@Override
 	public void powerOff() {
 		System.out.println("SamsungTv---전원 끄다");
 	}
+	
 	@Override
 	public void volumeUp() {
-		System.out.println("SamsungTv---소리 올린다.");
+//		System.out.println("SamsungTv---소리 올린다.");
+//		speaker = new SonySpeaker();
+		speaker.volumeUp();
 	}
+	
 	@Override
 	public void volumeDown() {
-		System.out.println("SamsungTv---소리 내린다.");
+//		System.out.println("SamsungTv---소리 내린다.");
+//		speaker = new SonySpeaker();
+		speaker.volumeDown();
 	}
 }
---
+++ src/main/java/polymorphism/SonySpeaker.java
@@ -0,0 +1,15 @@
+package polymorphism;
+
+public class SonySpeaker {
+	public SonySpeaker() {
+		System.out.println("===> SoneySpeaker 생성자-객체 생성");
+	}
+	
+	public void volumeUp() {
+		System.out.println("SonySpaeker---소리 율린다.");
+	}
+	
+	public void volumeDown() {
+		System.out.println("SoneySpeaker---소리 내린다.");
+	}
+}
